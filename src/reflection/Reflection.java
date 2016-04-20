3d142fafc9807933b1230a0d134ee044b8bf5e38
Kelsey Hyde <kelseyhyde@gmail.com>
2016-04-20 12:26:11 +1200

    Added logic to printClassModifiers method.

--- src/reflection/Reflection.java
+++ src/reflection/Reflection.java
@@ -11,15 +11,32 @@ public class Reflection {
     public static void main(String[] args) throws NoSuchFieldException, ClassNotFoundException{
         Class myClass = SortableBook.class;
         printPackage(myClass); 
+        printClassModifiers(myClass);
           
     }
     
     private static void printPackage(Class c){
-        System.out.println(c.getPackage() + ";");
+        Package classPackage = c.getPackage();
+        System.out.println(classPackage + ";");
     }
     
-    private static void printModifiers(Class c){
+    private static void printClassModifiers(Class c){
+        int modifiers = c.getModifiers();
+        if (Modifier.isPublic(modifiers))
+            System.out.print("public ");
+        else if (Modifier.isProtected(modifiers))
+            System.out.print("protected ");
+        else if (Modifier.isPrivate(modifiers))
+            System.out.print("private ");
+
+        if (Modifier.isAbstract(modifiers))
+            System.out.print("abstract ");
+        if (Modifier.isStatic(modifiers))
+            System.out.print("static ");
+        if (Modifier.isFinal(modifiers))
+            System.out.print("final ");
         
+        System.out.println();
     }
     
     private static void printClassHeader(Class c){
